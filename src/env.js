3cc0c65c2c4b074e816e1bc2746b368f108368bc
kolvra <sabrisirajkholbiabby@gmail.com>
2026-03-04 20:07:09 +0700

    chore: install required dependencies and env

--- src/env.js
+++ src/env.js
@@ -15,6 +15,7 @@ export const env = createEnv({
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
+    VIDEOSDK_SECRET_KEY: z.string(),
   },
 
   /**
@@ -23,7 +24,12 @@ export const env = createEnv({
    * `NEXT_PUBLIC_`.
    */
   client: {
-    // NEXT_PUBLIC_CLIENTVAR: z.string(),
+    NEXT_PUBLIC_PINATA_JWT: z.string(),
+    NEXT_PUBLIC_GATEWAY_URL: z.string(),
+    NEXT_PUBLIC_VIDEOSDK_API_KEY: z.string(),
+    NEXT_PUBLIC_ONCHAINKIT_API_KEY: z.string(),
+    NEXT_PUBLIC_FACTORY_ADDRESS: z.string(),
+    NEXT_PUBLIC_IDRX_ADDRESS: z.string(),
   },
 
   /**
@@ -34,6 +40,13 @@ export const env = createEnv({
     AUTH_SECRET: process.env.AUTH_SECRET,
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
+    VIDEOSDK_SECRET_KEY: process.env.VIDEOSDK_SECRET_KEY,
+    NEXT_PUBLIC_PINATA_JWT: process.env.NEXT_PUBLIC_PINATA_JWT,
+    NEXT_PUBLIC_GATEWAY_URL: process.env.NEXT_PUBLIC_GATEWAY_URL,
+    NEXT_PUBLIC_VIDEOSDK_API_KEY: process.env.NEXT_PUBLIC_VIDEOSDK_API_KEY,
+    NEXT_PUBLIC_ONCHAINKIT_API_KEY: process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY,
+    NEXT_PUBLIC_FACTORY_ADDRESS: process.env.NEXT_PUBLIC_FACTORY_ADDRESS,
+    NEXT_PUBLIC_IDRX_ADDRESS: process.env.NEXT_PUBLIC_IDRX_ADDRESS,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
