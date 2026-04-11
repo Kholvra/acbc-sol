80994dc6c83c2ae5335f10e12c932a35e591a850
kolvra <sabrisirajkholbiabby@gmail.com>
2026-04-11 10:44:35 +0700

    feat: wire up agreement and invoice routers
    - Import agreementRouter and invoiceRouter
    - Add agreement: agreementRouter to appRouter
    - Add invoice: invoiceRouter to appRouter
    - Make routers available via api.agreement.* and api.invoice.*

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -2,6 +2,8 @@ import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 import { campaignRouter } from "~/server/api/routers/campaign";
 import { kycRouter } from "~/server/api/routers/kyc";
 import { userRouter } from "./routers/user";
+import { agreementRouter } from "./routers/agreement";
+import { invoiceRouter } from "./routers/invoice";
 
 /**
  * This is the primary router for your server.
@@ -12,6 +14,8 @@ export const appRouter = createTRPCRouter({
   campaign: campaignRouter,
   kyc: kycRouter,
   user: userRouter,
+  agreement: agreementRouter,
+  invoice: invoiceRouter,
 });
 
 // export type definition of API
